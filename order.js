d712be3a9d1153ab609fda51c9acdfe56ba190f0
Bruno Martins da Cruz <bruno-martinslife@hotmail.com>
2024-03-02 02:46:07 -0300

    add scripts

---
+++ order.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Curso Web design',
+        productNumber: '44632',
+        paymentStatus: 'Crédito',
+        status: 'Feito'
+    },
+    {
+        productName: 'E-book UI/UX',
+        productNumber: '89234',
+        paymentStatus: 'PIX',
+        status: 'Reembolso'
+    },
+    {
+        productName: 'Assinatura Premium ERP',
+        productNumber: '52382',
+        paymentStatus: 'Crédito',
+        status: 'Feito'
+    },
+]
